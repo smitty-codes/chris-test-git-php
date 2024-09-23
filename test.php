5e7739c8a25d14636e5d48105671ade30d6ec47f
Chris Smith <smitty.st0ic@gmail.com>
2024-09-22 22:23:45 -0700

    changing how we pull env variables

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ echo "php rocks";
 echo "<br>";
 //echo phpinfo();
 // List all environment variables
-foreach ($_ENV as $key => $value) {
+$env_vars = getenv();
+foreach ($env_vars as $key => $value) {
     echo "$key => $value\n";
 }
