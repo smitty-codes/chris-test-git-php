1ecf72119299f460b57a45966a69417533ba07d8
Chris Smith <smitty.st0ic@gmail.com>
2024-09-22 22:22:01 -0700

    print list of environment variables

--- test.php
+++ test.php
@@ -3,5 +3,8 @@ echo "adding a line of code so it can be stashed";
 echo "<br>";
 echo "php rocks";
 echo "<br>";
-echo phpinfo();
-
+//echo phpinfo();
+// List all environment variables
+foreach ($_ENV as $key => $value) {
+    echo "$key => $value\n";
+}
