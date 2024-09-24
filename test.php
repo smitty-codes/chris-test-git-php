03a44040f7d2fdb69b8f1b4b3c6c45ce80e26d0e
Chris Smith <smitty.st0ic@gmail.com>
2024-09-23 22:19:10 -0700

    make font size bigger in a loop

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
+echo "<!DOCTYPE html><html><body>";
 echo "adding a line of code so it can be stashed";
 echo "<br>";
 echo "php rocks";
 echo "<br>";
 //echo phpinfo();
-phpinfo(INFO_ENVIRONMENT);
+#phpinfo(INFO_ENVIRONMENT);
+for ($i = 1; $i <= 10; $i++) {
+    echo "<p style=\"font-size:".$i."px\">testing</p><br>";
+}
+echo "</body></html>";
