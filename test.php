b3c8043f06efd066c0f4960453dcc2a697c906f2
Chris Smith <smitty.st0ic@gmail.com>
2024-09-28 16:21:07 -0700

    removing old echo lines from main

--- test.php
+++ test.php
@@ -1,9 +1,5 @@
 <?php
 echo "<!DOCTYPE html><html><body>";
-echo "adding a line of code so it can be stashed";
-echo "<br>";
-echo "php rocks";
-echo "<br>";
 //echo phpinfo();
 #phpinfo(INFO_ENVIRONMENT);
 for ($i = 1; $i <= 20; $i++) {
