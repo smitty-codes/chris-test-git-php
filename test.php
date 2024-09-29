a1b232ba4d60fcfaf2cdeab82ad1943c2e0d99ec
Chris Smith <smitty.st0ic@gmail.com>
2024-09-28 18:36:25 -0700

    changing the text that is printed out

--- test.php
+++ test.php
@@ -3,10 +3,13 @@ echo "<!DOCTYPE html><html lang='en'><body style='background-color: blue'>";
 //echo phpinfo();
 #phpinfo(INFO_ENVIRONMENT);
 for ($i = 1; $i <= 20; $i++) {
-    echo "<p style=\"font-size:".$i."px\">testing</p><br>";
+    echo "<p style=\"font-size:".$i."px\">make big</p><br>";
 }
 for ($i = 20; $i >= 0; $i--) {
-    echo "<p style=\"font-size:".$i."px\">testing</p><br>";
+    echo "<p style=\"font-size:".$i."px\">make small</p><br>";
+}
+for ($i = 1; $i <= 20; $i++) {
+    echo "<p style=\"font-size:".$i."px\">make big</p><br>";
 }
 echo "loop complete";
 echo "</body></html>";
